e8aa17abe34c2fd1ccb46ef8889fa5dde04d57ff
AniElaina <ani.elaina.carr@gmail.com>
2022-03-14 20:49:31 -0400

    first commit

---
+++ ModernJavaScript.js
@@ -0,0 +1,34 @@
+// let age = 26;
+// age = 27;
+
+// age = age + 1;
+
+// let health = 0.65;
+
+// let isAlive = true;
+// isAlive = false;
+
+// isAlive = 5 > 2;
+
+// const pi = 3.14159;
+// let numberOfLimbs = 4;
+
+// console.log(health);
+
+// var name = "Nimish";
+// var zenva = "Zenva";
+
+// var age = 26;
+
+// //string interpelation
+// let finalString = `Nimish is ${age} age years old`;
+
+// console.log(finalString);
+
+// zenva = zenva.padEnd(8, "."); //Zenva...
+
+// console.log(zenva);
+
+const apiKey = "E134E90D9AB67809526B537ABCF67C9C";
+
+const appId = "240";
